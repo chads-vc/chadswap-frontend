ff1059272cae5ff8d816afb157b8898eb0a93c02
Daiki Saito <daiki.saito92@gmail.com>
2020-11-04 05:08:01 +1000

    add 'fontWeight' prop

--- src/components/Label/Label.tsx
+++ src/components/Label/Label.tsx
@@ -5,21 +5,24 @@ interface LabelProps {
   text?: string
   horizontal?: boolean
   fontSize?: number
+  fontWeight?: number
 }
 
-const Label: React.FC<LabelProps> = ({ text, horizontal, fontSize }) => (
-  <StyledLabel horizontal={horizontal} fontSize={fontSize}>{text}</StyledLabel>
+const Label: React.FC<LabelProps> = ({ text, horizontal, fontSize, fontWeight }) => (
+  <StyledLabel horizontal={horizontal} fontSize={fontSize} fontWeight={fontWeight}>{text}</StyledLabel>
 )
 
 interface StyledLabelProps {
   horizontal?: boolean,
-  fontSize?: number
+  fontSize?: number,
+  fontWeight?: number,
 }
 
 const StyledLabel = styled.div<StyledLabelProps>`
   color: ${(props) => props.theme.color.purple};
   width: ${(props) => props.horizontal ? "50%": "100%"};
   font-size: ${(props) => props.fontSize}px;
+  font-weight: ${(props) => props.fontWeight};
 `
 
 export default Label
